caba2c7e5d8c6c3660f419a98f1e71a63f3c502d
dangorbey <diggerpro45@gmail.com>
2023-09-20 16:13:19 -0400

    resolve type errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,6 +59,8 @@ export default function Home() {
     }
   ];
 
+  let selectedColor = selected !== null ? colors[selected] : undefined;
+
 
   return (
     <>
@@ -68,7 +70,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div style={{ backgroundColor: selected !== null ? colors[selected].base : 'initial' }}>
+        <div style={{
+          backgroundColor: selectedColor?.base || 'initial'
+        }}>
           Highlight Colors
         </div>
 
