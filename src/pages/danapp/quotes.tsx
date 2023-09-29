bc83ae6caeea79b278348b647f9e424b7f19defe
dangorbey <diggerpro45@gmail.com>
2023-09-29 18:08:19 -0400

    some css stuff

--- src/pages/danapp/quotes.tsx
+++ src/pages/danapp/quotes.tsx
@@ -1,24 +1,35 @@
 import Link from "next/link";
 import Navbar from "~/components/Navbar";
 import { api } from "~/utils/api";
+import styles from "./quotes.module.css";
 
 function QuoteListPage() {
   const { data, isLoading } = api.quotes.getUserQuotes.useQuery();
+
+  // Function to highlight content within asterisks
+  const formatQuoteContent = (content: string) => {
+    // Using regex to replace **some content** with <span className={styles.highlight}>some content</span>
+    return content.replace(/\*\*(.*?)\*\*/g, `<span class="${styles.highlight}">$1</span>`);
+  };
 
   if (isLoading) return <div>Loading...</div>;
   if (!data) return <div>Something went wrong</div>;
 
   return (
     <div>
-      <div>
-        <Navbar />
+      <Navbar />
+      {isLoading && <div>Loading...</div>}
+      {!data && <div>Something went wrong</div>}
+      <div className={styles.container}>
+        {data?.map((fullQuote) => (
+          <div className={styles.quote} key={fullQuote.quote.id}>
+            {/* Using dangerouslySetInnerHTML to render the formatted content */}
+            <div dangerouslySetInnerHTML={{ __html: formatQuoteContent(fullQuote.quote.content) }}></div>
+            <div style={{ height: '10px' }}></div>
+            <Link className={styles.edit} href={`/danapp/edit/${fullQuote.quote.id}`}>Edit</Link>
+          </div>
+        ))}
       </div>
-      {data.map((fullQuote) => (
-        <div key={fullQuote.quote.id}>
-          {fullQuote.quote.content}
-          <Link href={`/danapp/edit/${fullQuote.quote.id}`}>Edit</Link>
-        </div>
-      ))}
     </div>
   );
 }
