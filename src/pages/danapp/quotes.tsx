befe26bd6fa255b29bba9478e1f3e6d880fe5dc8
dangorbey <diggerpro45@gmail.com>
2023-10-02 23:51:31 -0400

    added a create button

--- src/pages/danapp/quotes.tsx
+++ src/pages/danapp/quotes.tsx
@@ -2,9 +2,43 @@ import Link from "next/link";
 import Navbar from "~/components/Navbar";
 import { api } from "~/utils/api";
 import styles from "./quotes.module.css";
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/router";
+
+const defaultQuote = "1. Click to edit!\n\n2. Any text you place **within two sets of asterisks** will be highlighted!\n\n3. Have fun!"
 
 function QuoteListPage() {
   const { data, isLoading } = api.quotes.getUserQuotes.useQuery();
+  const { mutate } = api.quotes.create.useMutation();
+  const router = useRouter();
+  const { user } = useUser();
+
+  const handleCreateNewQuote = async () => {
+    if (!user) return;
+
+    try {
+      // Mutate without waiting for a return value.
+      await mutate(
+        { content: defaultQuote },
+        {
+          onSuccess: (data) => {
+            // Assuming the response data has the id of the new quote
+            if (data && data.id) {
+              router.push(`/danapp/edit/${data.id}`);
+            } else {
+              console.error("Failed to get the ID of the new quote.");
+            }
+          },
+          onError: (error) => {
+            console.error("Error creating a new quote:", error);
+          }
+        }
+      );
+    } catch (error) {
+      console.error("Mutation error:", error);
+    }
+  };
+
 
   const formatQuoteContent = (content: string, highlightColor: string) => {
     const parts = content.split(/\*\*(.*?)\*\*/).map((part, index) =>
@@ -27,6 +61,10 @@ function QuoteListPage() {
     <div>
       <Navbar />
 
+      <div className={styles.createButtonContainer}>
+        <button className={styles.createButton} onClick={handleCreateNewQuote}>Create a new quote!</button>
+      </div>
+
       {isLoading && <div className={styles.loading}>Loading...</div>}
 
       {!data && !isLoading && <div>Something went wrong</div>}
@@ -35,7 +73,7 @@ function QuoteListPage() {
         <div className={styles.container}>
           {data.map((fullQuote) => (
             <div className={styles.quote} key={fullQuote.quote.id}>
-              <div>{formatQuoteContent(fullQuote.quote.content, fullQuote.quote.highlightColor || "#FFFF77")}</div>
+              <div>{formatQuoteContent(fullQuote.quote.content, fullQuote.quote.highlightColor ?? "#FFFF77")}</div>
               <div style={{ height: '10px' }}></div>
               <Link className={styles.edit} href={`/danapp/edit/${fullQuote.quote.id}`}>Edit</Link>
             </div>
