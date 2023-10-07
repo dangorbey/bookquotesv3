8c8aed1d3b8fead69314f4da1a2838c17fbb9d4a
dangorbey <diggerpro45@gmail.com>
2023-10-07 13:59:49 -0400

    css quote style

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function MyDocument() {
+  return (
+    <Html>
+      <Head>
+        <link rel="stylesheet" href="https://use.typekit.net/fhn2cew.css" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- src/pages/danapp/vision.tsx
+++ src/pages/danapp/vision.tsx
@@ -1,23 +1,34 @@
-import Image from 'next/image'
 import Navbar from "~/components/Navbar";
-import pic from '/public/Quote Ref-003.png'
-import styles from './vision.module.css'
+import styles from './vision.module.css';
+
+function surroundWithLorem(): JSX.Element {
+  const lorem = "Nullam tempus eget erat a scelerisque. Curabitur ut dolor id est eleifend eleifend ut ut nibh. Nulla leo neque, faucibus non nulla vel, blandit convallis nunc. Pellentesque quis blandit libero. ";
+
+  return (
+    <div className={styles.textFrame}>
+      <div className={styles.content}>
+        <div className={styles.blur}>{lorem}</div>
+        <div className={styles.quote}>Thus in love the free-lovers say: ‘Let us have the splendor of offering ourselves without the peril of committing ourselves; let us see whether <span className={styles.highlight}>one cannot commit suicide an unlimited number of times.</span></div>
+        <div className={styles.blur}>{lorem}</div>
+      </div>
+      <div className={`${styles.content} ${styles.mirror}`}>
+        <div>{lorem + lorem + lorem}</div>
+      </div>
+    </div>
+  );
+}
 
 export default function Page() {
   return (
     <div>
-      <Navbar />
-      <div className={styles.container}>
-        <h1 className={styles.imageTitle}>v1 visual style:</h1>
-        <div className={styles.imageWrapper}>
-          <Image
-            src={pic}
-            alt='Quote Visual Reference'
-            className={styles.quote}
-            placeholder="blur"
-          />
+      <div>
+        <Navbar />
+        <div className={styles.container}>
+          <div className={styles.frame}>
+            {surroundWithLorem()}
+          </div>
         </div>
-      </div>
+      </div >
     </div>
   )
 }
